7ce70788bbaee4971423eaf0f1b88bdce7999f37
202211035 <112609961+202211035@users.noreply.github.com>
2023-01-05 19:13:53 +0900

    Add files via upload

---
+++ 20230105/_1.ino
@@ -0,0 +1,41 @@
+// C++ code
+//
+
+int tones[] = {523,587,659,698,784,880,988,1046};
+
+void setup()
+{
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  tone(12, tones[4], 500); 
+  delay(1000);		  
+  tone(12, tones[4], 500); 
+  delay(1000);		 
+  tone(12, tones[5], 500); 
+  delay(1000);		  
+  tone(12, tones[5], 500); 
+  delay(1000);		 
+  
+  tone(12, tones[4], 500); 
+  delay(1000);		  
+  tone(12, tones[4], 500); 
+  delay(1000);		  
+  tone(12, tones[2], 1000); 
+  delay(1000);		  
+  
+  tone(12, tones[4], 500); 
+  delay(1000);		  
+  tone(12, tones[4], 500); 
+  delay(1000);		  
+  tone(12, tones[2], 500);
+  delay(1000);		  
+  tone(12, tones[2], 500);
+  delay(1000);		  
+  tone(12, tones[1], 1500);
+  delay(3000);		 
+  
+  
+}
